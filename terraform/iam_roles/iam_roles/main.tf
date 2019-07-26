75b98760d12a8abf917d55a2b9bd04274981b717
Jessica Vilaysak <jessica.vilaysak@nab.com.au>
2019-07-26 13:03:13 +1000

    removing unnecessary vars

--- terraform/iam_roles/iam_roles/main.tf
+++ terraform/iam_roles/iam_roles/main.tf
@@ -2,6 +2,21 @@
 
 resource "aws_iam_role" "bastion_iam_role" {
   name               = "${var.bastion_iam_role_name}"
+  assume_role_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  }
+  EOF
 }
 
 
