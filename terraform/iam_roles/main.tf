926334fe39a396f624d056a88191acda2aa7fc61
Jessica Vilaysak <jessica.vilaysak@nab.com.au>
2019-07-26 12:48:47 +1000

    removing unnecessary vars

--- terraform/iam_roles/main.tf
+++ terraform/iam_roles/main.tf
@@ -2,20 +2,14 @@
 #----------------security module ---------------------
 module "iam_roles" {
   source             = "./iam_roles"
-  tags_environment   = "${var.tags_environment}"
-  key_name           = "${var.key_name}"
-  public_key_path    = "${var.public_key_path}"
-  private_key_path   = "${var.private_key_path}"
-  jenkins_kms_alias  = "${var.jenkins_kms_alias}"
-  tags_costcentre    = "${var.tags_costcentre}"
-  tags_owner         = "${var.tags_owner}"
-  tags_applicationID = "${var.tags_applicationID}"
+  bastion_iam_role_name     = "${var.bastion_iam_role_name}"
+  bastion_iam_profile_name  = "${var.bastion_iam_profile_name}"
 }
 
 terraform {
   backend "s3" {
-    bucket = "9328-test-bucket"
-    key    = "jenkins-poc-stack/terraform.tfstate"
-    region = "ap-southeast-2"
+    bucket = "${s3_backend_bucket}"
+    key    = "${s3_backend_bucket_path}"
+    region = "${region}"
   }
 }
--- terraform/iam_roles/terraform.tfvars
+++ terraform/iam_roles/terraform.tfvars
@@ -1,4 +1,15 @@
+## Any global vars
+region = "ap-southeast-2"
+
+
+## Vars for the iam_roles module
 
 bastion_iam_role_name = "BastionInstanceRole"
 bastion_iam_profile_name = "BastionInstanceProfile"
 
+
+
+
+## Vars for S3 backend
+s3_backend_bucket = "9328-test-bucket"
+s3_backend_bucket_path = "jenkins-poc-stack/terraform.tfstate"
