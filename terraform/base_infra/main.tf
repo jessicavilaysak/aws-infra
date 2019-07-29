8154e02deda73febeac2986ac8eb6b232c5c10f7
Jessica Vilaysak <jessica.vilaysak@nab.com.au>
2019-07-29 11:56:11 +1000

    adding security groups

--- terraform/base_infra/main.tf
+++ terraform/base_infra/main.tf
@@ -8,6 +8,18 @@ module "roles" {
   jenkinsmaster_iam_profile_name = "${var.jenkinsmaster_iam_profile_name}"
 }
 
+module "security_groups" {
+  source = "./security_groups"
+  ssh_port = "${var.ssh_port}"
+  tools_vpc_id = "${var.tools_vpc_id}"
+  bastion_tooling_sg_name = "${var.bastion_tooling_sg_name}"
+  jenkins_tooling_sg_name = "${var.jenkins_tooling_sg_name}"
+  all_inbound_cidrs = "${var.all_inbound_cidrs}"
+  all_outbound_cidrs = "${var.all_outbound_cidrs}"
+  http_port = "${var.http_port}"
+  https_port = "${var.https_port}"
+}
+
 
 provider "aws" {
   region = "ap-southeast-2"
