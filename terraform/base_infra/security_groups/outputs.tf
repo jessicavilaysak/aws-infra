8203230cd3a74f983ebcd658865288f29a91f26d
Jessica Vilaysak <jessica.vilaysak@nab.com.au>
2019-07-29 12:05:58 +1000

    modify sg output to show id

--- terraform/base_infra/security_groups/outputs.tf
+++ terraform/base_infra/security_groups/outputs.tf
@@ -1,14 +1,14 @@
 #-------------------root/outputs.tf file ------------------
 
 output "bastion_sg" {
-  value = "${aws_security_group.bastion_tooling_sg}"
+  value = "${aws_security_group.bastion_tooling_sg.id}"
 }
 
 output "jenkins_sg" {
-  value = "${aws_security_group.jenkins_tooling_sg}"
+  value = "${aws_security_group.jenkins_tooling_sg.id}"
 }
 
 output "jenkins_lb_sg"{
-  value = "${aws_security_group.jenkins_tooling_lb_sg}"
+  value = "${aws_security_group.jenkins_tooling_lb_sg.id}"
 }
 
