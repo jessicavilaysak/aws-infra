fe8c5a1873fd9d33d6400f7c81d5f0ecca3e6c93
Jessica Vilaysak <jessica.vilaysak@nab.com.au>
2019-07-29 16:48:04 +1000

    adding asg and launch config

--- terraform/base_infra/roles/variables.tf
+++ terraform/base_infra/roles/variables.tf
@@ -1,4 +1,4 @@
 variable "bastion_iam_role_name" {}
 variable "bastion_iam_profile_name" {}
-variable "jenkinsmaster_iam_role_name" {}
-variable "jenkinsmaster_iam_profile_name" {}
+variable "jenkins_iam_role_name" {}
+variable "jenkins_iam_profile_name" {}
